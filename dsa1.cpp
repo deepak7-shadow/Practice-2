81b9e079d8ef216e948e3abb9216feb21c099aa5
deepak7-shadow <deepak843161.438@gmail.com>
2026-08-14 21:40:51 +0530

    All

---
+++ dsa1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    cout << "Original array: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nReversed array: ";
+    for (int i = 4; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
