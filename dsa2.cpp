4f04052b05ceaf926ba587157b132b9a854a4ebd
deepak7-shadow <deepak843161.438@gmail.com>
2026-08-14 21:42:47 +0530

    All

---
+++ dsa2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int stack[5];
+    int top = -1;
+
+    // Push elements
+    stack[++top] = 10;
+    stack[++top] = 20;
+    stack[++top] = 30;
+
+    cout << "Stack elements: ";
+    for (int i = top; i >= 0; i--)
+    {
+        cout << stack[i] << " ";
+    }
+
+    // Pop an element
+    cout << "\nPopped element: " << stack[top--] << endl;
+
+    cout << "Stack after pop: ";
+    for (int i = top; i >= 0; i--)
+    {
+        cout << stack[i] << " ";
+    }
+
+    return 0;
+}
