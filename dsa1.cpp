ae2445025512b72619470341a08b310a6245747b
Deepak <deepak843161.438@gmail.com>
2026-08-20 22:31:25 +0530

    Fix array iteration to include all elements

--- dsa1.cpp
+++ dsa1.cpp
@@ -6,7 +6,7 @@ int main()
     int arr[5] = {10, 20, 30, 40, 50, 60};
 
     cout << "Original array: ";
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 6; i++)
     {
         cout << arr[i] << " ";
     }
